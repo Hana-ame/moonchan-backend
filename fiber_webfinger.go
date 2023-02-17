ce7f728fcc81306ff44663b8a0479d3fcd8194e4
lumino <luminovoez@gmail.com>
2023-02-17 14:20:52 +0800

    change filename

---
+++ fiber_webfinger.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/Hana-ame/moonchan-backend/db"
+	"github.com/Hana-ame/moonchan-backend/webfinger"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func webfingerApp() *fiber.App {
+	app := fiber.New()
+
+	app.All("", func(c *fiber.Ctx) error {
+		acct := c.Query("resource")
+
+		username, domain := webfinger.ParseAcct(acct)
+
+		if !webfinger.CheckDomain(domain) {
+			return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+				"error": "invalid_resource",
+			})
+		}
+
+		if !db.CheckUserExist(username) {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": "no_such_user",
+			})
+		}
+
+		err := c.Status(fiber.StatusOK).JSON(webfinger.GetResource(username, domain))
+
+		return err
+	})
+
+	return app
+}
